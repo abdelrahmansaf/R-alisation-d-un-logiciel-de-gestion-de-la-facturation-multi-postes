30fc356ff6c2a7cf5b23100e7499bebc10f70f9e
abdelrahmansaf <77361942+abdelrahmansaf@users.noreply.github.com>
2021-02-04 09:08:42 +0100

    Update scriptwindows.bat

--- scriptwindows.bat
+++ scriptwindows.bat
@@ -1,10 +1,11 @@
+pip install mysql-connector-python
 set/p ip=Read-host -Prompt 'Tapez votre nom_serveur@192.168.1.XXX :'
 ssh %ip% "mkdir -p ~/.ssh/"
 type C:\Users\utilisateur\.ssh\id_rsa.pub | ssh %ip% "cat >> ~/.ssh/authorized_keys"
-scp C:\Users\utilisateur\Documents\svr_install\script_bash.sh %ip%:./script_bash.sh
-scp C:\Users\utilisateur\Documents\svr_install\db_comptable.sql %ip%:./database.sql
+scp C:\Users\utilisateur\Desktop\script\script_bash.sh %ip%:./script_bash.sh
+scp C:\Users\utilisateur\Desktop\script\database.sql %ip%:./database.sql
 ssh %ip% "sed -i -e 's/\r$//' ./script_bash.sh"
-ssh %ip% "chmod 777 ./script_bash.sh"
+ssh %ip% "chmod 600 ./script_bash.sh"
 ssh %ip% "chmod 777 ./database.sql"
-pip install mysql-connector-python
+ssh -t %ip% sudo sh ./script_bash.sh
 ssh %ip%
